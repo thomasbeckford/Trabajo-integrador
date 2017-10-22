6befa25cc561678cd0ab33f193c24d3ae107c221
tebeck <tebeckford@gmail.com>
2017-10-22 01:28:59 -0300

    index

--- index.php
+++ index.php
@@ -4,57 +4,103 @@
 <?php include("./shared/navbar.php");?>
   <script src="./assets/js/sweetAlertMsg.js"></script>
       </head>
-          <div class="animated fadeIn fast">
-      <body>
-        <!-- Swiper -->
-        <div class="swiper-container">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide bg-secondary"><div class="container " ><h2>La nueva tienta de compra y venta online mas rapida, donde podes contactarte con tu comprador y o vendedor de manera segura.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-danger disabled" name="button">Que estas esperando?</button></div></div></div>
-            <div class="swiper-slide bg-secondary"><div class="container "><h2>Ofertas de la semana</h2><h3>Conocé los productos en que están en descuento y ahorrá con nosotros. Entrá ya y descubrí todas las increíbles ofertas que tenemos para vos y tu familia. </h3></div></div>
-            <div class="swiper-slide bg-secondary"><div class="container " ><h2>Encontra hasta 50% de descuento en la seccion de camaras, tablets, celulares y sus distintos tipos de accesorios.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-primary disabled" name="button">Mira los descuentos!</button></div></div></div>
-            <div class="swiper-slide bg-secondary"><div class="container " ><h2>Aliados con MercadoPago, la plataforma de pagos online mas segura del pais. Recibi tu dinero al instante y tus pagos sin costos ocultos.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-warning disabled" name="button">Conoce mas acerca de nosotros</button></div></div></div>
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
+        <div class="animated fadeIn fast">
+          <body>
+            <!-- Swiper -->
+            <div class="swiper-container">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide bg-secondary"><div class="container " ><h2>La nueva tienta de compra y venta online mas rapida, donde podes contactarte con tu comprador y o vendedor de manera segura.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-danger disabled" name="button">Que estas esperando?</button></div></div></div>
+                <div class="swiper-slide bg-secondary"><div class="container "><h2>Ofertas de la semana</h2><h3>Conocé los productos en que están en descuento y ahorrá con nosotros. Entrá ya y descubrí todas las increíbles ofertas que tenemos para vos y tu familia. </h3></div></div>
+                <div class="swiper-slide bg-secondary"><div class="container " ><h2>Encontra hasta 50% de descuento en la seccion de camaras, tablets, celulares y sus distintos tipos de accesorios.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-primary disabled" name="button">Mira los descuentos!</button></div></div></div>
+                <div class="swiper-slide bg-secondary"><div class="container " ><h2>Aliados con MercadoPago, la plataforma de pagos online mas segura del pais. Recibi tu dinero al instante y tus pagos sin costos ocultos.</h2><br><br><div class="container col-md-3"><button type="button" class="btn btn-block btn-warning disabled" name="button">Conoce mas acerca de nosotros</button></div></div></div>
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
 
 
 <div class="main-container container">
 <div class="row ">
-
-  <div class="card col-md-3">
+  <div class="card-deck">
+    <div class="card">
+      <img class="card-img-top rounded-top " src="http://lorempixel.com/239/200/" >
       <div class="card-body">
-        <h4 class="card-title border-top-0">Memoria RAM</h4>
-        <p class="card-text">Nueva memoria ram DDR5, auge del gamming.</p>
-        <img class="card-img-top rounded-top" src="http://lorempixel.com/technics/200/100/" >
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
-  </div>
-  <div class="card col-md-3">
-      <div class="card-body">
-        <h4 class="card-title border-top-0">Memoria RAM</h4>
-        <p class="card-text">Nueva memoria ram DDR5, auge del gamming.</p>
-        <img class="card-img-top rounded-top" src="http://lorempixel.com/transport/200/100/" >
-      </div>
-  </div>
-  <div class="card col-md-3">
-      <div class="card-body">
-        <h4 class="card-title border-top-0">Memoria RAM</h4>
-        <p class="card-text">Nueva memoria ram DDR5, auge del gamming.</p>
-        <img class="card-img-top rounded-top" src="http://lorempixel.com/people/200/100/" >
-      </div>
-  </div>
-  <div class="card col-md-3">
-      <div class="card-body">
-        <h4 class="card-title border-top-0">Memoria RAM</h4>
-        <p class="card-text">Nueva memoria ram DDR5, auge del gamming.</p>
-        <img class="card-img-top rounded-top" src="http://lorempixel.com/city/200/100/" >
-      </div>
-  </div>
-
-
-</div>
-</div>
-
     </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/sports/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/technology/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/technology/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="main-container container">
+<div class="row ">
+  <div class="card-deck">
+    <div class="card">
+      <img class="card-img-top rounded-top " src="http://lorempixel.com/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/sports/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-top" src="http://lorempixel.com/technology/239/200/" >
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+</div>
 
   <script src="./assets/js/swiperConfig.js"></script>
 
